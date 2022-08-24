15f160a0d816f07947e62751d0c6f1a1b2e1d80d
davidhieber <david.hieber@uni-tuebingen.de>
2022-08-24 11:04:53 +0200

    moved funktion to utils to make code mor readabel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
-from utils import get_list_of_folders
+from utils import get_list_of_folders, mkdir_when_not_existent, make_list_of_dirs_in_path, \
+    check_if_text_is_a_date_and_output_in_isofromat
 import  os
 
 def main():
@@ -16,38 +17,26 @@ def main():
     same proces is repeated fo the niff folder, but only select secenses get copied
     :return:
     """
-    #origin_path = "/home/davidhieber/Documents/Leucoexpert/test_folder_mris/orgin"
-    #target_path = "/home/davidhieber/Documents/Leucoexpert/test_folder_mris/target"
+    #get the path from the args
     origin_path = sys.argv[1]
     target_path = sys.argv[2]
+
     if os.name == "nt":
         # create the dicom and nifti folder in the target folder if they do not exist
-        if not os.path.exists(target_path + r"\\dicom"):
-            os.mkdir(target_path + r"\\dicom")
-        if not os.path.exists(target_path + r"\\nifti"):
-            os.mkdir(target_path + r"\\nifti")
-
+        mkdir_when_not_existent(target_path + r"\\dicom")
+        mkdir_when_not_existent(target_path + r"\\nifti")
         patient_folders = get_list_of_folders(origin_path)
         # create the same folders in the target folder if they do not exist
-        for patient_folder in patient_folders:
-            if not os.path.exists(target_path + r"\\dicom\\" + patient_folder):
-                os.mkdir(target_path + r"\\dicom\\" + patient_folder)
-            if not os.path.exists(target_path + r"\\nifti\\" + patient_folder):
-                os.mkdir(target_path + r"\\nifti\\" + patient_folder)
+        make_list_of_dirs_in_path(target_path + r"\\dicom\\", patient_folders)
+        make_list_of_dirs_in_path(target_path + r"\\nifti\\", patient_folders)
     else:
         # create the dicom and nifti folder in the target folder if they do not exist
-        if not os.path.exists(target_path + "/dicom"):
-            os.mkdir(target_path + "/dicom")
-        if not os.path.exists(target_path + "/nifti"):
-            os.mkdir(target_path + "/nifti")
-
+        mkdir_when_not_existent(target_path + "/dicom")
+        mkdir_when_not_existent(target_path + "/nifti")
         patient_folders = get_list_of_folders(origin_path)
         # create the same folders in the target folder if they do not exist
-        for patient_folder in patient_folders:
-            if not os.path.exists(target_path + "/dicom/" + patient_folder):
-                os.mkdir(target_path + "/dicom/" + patient_folder)
-            if not os.path.exists(target_path + "/nifti/" + patient_folder):
-                os.mkdir(target_path + "/nifti/" + patient_folder)
+        make_list_of_dirs_in_path(target_path + "/dicom/", patient_folders)
+        make_list_of_dirs_in_path(target_path + "/nifti/", patient_folders)
 
     number_dicoms = 0
     # for every patient folder in the origin folder get the list of sub folder names, create a list of iso formatted dates
@@ -59,6 +48,7 @@ def main():
         for record_date in record_dates:
             # check if the recorde date containes a dicom study
             if os.name == "nt":
+
                 if not os.path.exists(
                         origin_path + r"\\" + patient_folder + r"\\" + record_date + r"\\dicom" ):
                     continue
@@ -69,28 +59,15 @@ def main():
                 record_date = record_date.split(r"\\")[-1]
             else:
                 record_date = record_date.split("/")[-1]
-            #remove all characters that are not numbers  from te sub folder name
-            record_date_iso = record_date.replace(" ", "")
-            record_date_iso = record_date_iso.replace("-", "")
-            record_date_iso = record_date_iso.replace(":", "")
-            record_date_iso = record_date_iso.replace(".", "")
-            #check if record_date is a valid date
-            if not record_date_iso.isnumeric():
-                print(f"not a valid date  {patient_folder}/{record_date}")
+            # check if text is a date and output in isofromat
+            record_date_iso = check_if_text_is_a_date_and_output_in_isofromat(record_date)
+            if record_date_iso == "":
                 continue
-            # check if the recorde date is a valid date
-            if not (1900 < int(record_date_iso[:4]) < 2100 and 1 <= int(record_date_iso[4:6]) <= 12 and 1 <= int(record_date_iso[6:8]) <= 31):
-                print(f"not a valid date {patient_folder}/{record_date}")
-                continue
-            # convert to iso formatted date
-            record_date_iso = record_date_iso[:4] + "-" + record_date_iso[4:6] + "-" + record_date_iso[6:8]
             # create a new folder with the iso formatted date as name in the patient folder of the target folder
             if os.name == "nt":
-                if not os.path.exists(target_path + r"\\dicom\\" + patient_folder + r"\\" + record_date_iso):
-                    os.mkdir(target_path + r"\\dicom\\" + patient_folder + r"\\" + record_date_iso)
+                mkdir_when_not_existent(target_path + r"\\dicom\\" + patient_folder + r"\\" + record_date_iso)
             else:
-                if not os.path.exists(target_path + "/dicom/" + patient_folder + "/" + record_date_iso):
-                    os.mkdir(target_path + "/dicom/" + patient_folder + "/" + record_date_iso)
+                mkdir_when_not_existent(target_path + "/dicom/" + patient_folder + "/" + record_date_iso)
             # check if the recorde date containes a dicom study
                 # copy the dicom folder to the new folder
                # check the os system and copy the dicom folder to the new folder
@@ -119,32 +96,15 @@ def main():
                     record_date = record_date.split(r"\\")[-1]
                 else:
                     record_date = record_date.split("/")[-1]
-                #remove all characters that are not numbers  from te sub folder name
-                record_date_iso = record_date.replace(" ", "")
-                record_date_iso = record_date_iso.replace("-", "")
-                record_date_iso = record_date_iso.replace(":", "")
-                record_date_iso = record_date_iso.replace(".", "")
-                #check if record_date is a valid date
-                if not record_date_iso.isnumeric():
-                    print(f"not a valid date  {patient_folder}/{record_date}")
+                record_date_iso = check_if_text_is_a_date_and_output_in_isofromat(record_date)
+                if record_date_iso == "":
                     continue
-                # check if the recorde date is a valid date
-                if not (1900 < int(record_date_iso[:4]) < 2100 and 1 <= int(record_date_iso[4:6]) <= 12 and 1 <= int(record_date_iso[6:8]) <= 31):
-                    print(f"not a valid date {patient_folder}/{record_date}")
-                    continue
-                # convert to iso formatted date
-                record_date_iso = record_date_iso[:4] + "-" + record_date_iso[4:6] + "-" + record_date_iso[6:8]
-
                 # create a new folder with the iso formatted date as name in the patient folder of the target folder
                 if os.name == "nt":
-                    if not os.path.exists(target_path + r"\nifti\\" + patient_folder + r"\\" + record_date_iso):
-                        os.mkdir(target_path + r"\nifti\\" + patient_folder + r"\\" + record_date_iso)
+                    mkdir_when_not_existent(target_path + r"\nifti\\" + patient_folder + r"\\" + record_date_iso)
                 else:
-                    if not os.path.exists(target_path + "/nifti/" + patient_folder + "/" + record_date_iso):
-                        os.mkdir(target_path + "/nifti/" + patient_folder + "/" + record_date_iso)
-                    # check if the recorde date containes a dicom study
+                    mkdir_when_not_existent(target_path + "/nifti/" + patient_folder + "/" + record_date_iso)
 
-               #
                # check the os system and copy the dicom folder to the new folder
                 if os.name == "nt":
                     os.system(r"copy " + origin_path + r"\\" + patient_folder + r"\\" + record_date + r"\\" + nifti + r" " + target_path + r"\\nifti\\" + patient_folder + r"\\" + record_date_iso + r"\\"+ nifti)
--- utils.py
+++ utils.py
@@ -8,3 +8,43 @@ def get_list_of_folders(path: str) -> list:
     """
     return [f.name for f in os.scandir(path) if f.is_dir()]
 
+def mkdir_when_not_existent(path: str) -> None:
+    """
+    This function creates a folder when it does not exist.
+    :param path:
+    :return:
+    """
+    if not os.path.exists(path):
+        os.mkdir(path)
+def make_list_of_dirs_in_path(path: str, names: list) -> None:
+    """
+    This function makes a dir for every name in the list of names at the given path.
+    :param path:
+    :return:
+    """
+    for name in names:
+        mkdir_when_not_existent(path + name)
+
+def check_if_text_is_a_date_and_output_in_isofromat(date: str) -> str:
+    """
+    This function checks if a text is a date and outputs it in iso format.
+    :param text:
+    :return:
+    """
+    # remove all characters that are not numbers  from te sub folder name
+    date_iso = date.replace(" ", "")
+    date_iso = date_iso.replace("-", "")
+    date_iso = date_iso.replace(":", "")
+    date_iso = date_iso.replace(".", "")
+    # check if record_date is numeric
+    if not date_iso.isnumeric():
+        print(f"date is not numeric  {date}")
+        return ""
+    # check if the recorde date is a valid date
+    if not (1900 < int(date_iso[:4]) < 2100 and 1 <= int(date_iso[4:6]) <= 12 and 1 <= int(
+            date_iso[6:8]) <= 31):
+        print(f"not a valid date {date}")
+        return ""
+    # convert to iso formatted date
+    date_iso = date_iso[:4] + "-" + date_iso[4:6] + "-" + date_iso[6:8]
+    return date_iso
